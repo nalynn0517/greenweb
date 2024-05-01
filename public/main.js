bda500b6f61675b9be1a71a0f77129432cbd1c55
nalynn0517 <nalynnxx@naver.com>
2024-05-01 11:17:30 +0900

    map

--- public/main.js
+++ public/main.js
@@ -29,6 +29,15 @@ function initMap() {
     // 지도 생성
     const map = new kakao.maps.Map(container, options);
 
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+
+    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+    
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
     // 제로 웨이스트 샵 정보를 가져와서 표시
     showZeroWasteStores(map);
 }
@@ -40,12 +49,12 @@ function showZeroWasteStores(map) {
 
     // 제로 웨이스트 샵 마커 이미지
     const markerImages = {
-        경기: new kakao.maps.MarkerImage('img/m1.png', new kakao.maps.Size(20, 30)),
-        서울: new kakao.maps.MarkerImage('img/m2.png', new kakao.maps.Size(20, 30)),
-        충청: new kakao.maps.MarkerImage('img/m3.png', new kakao.maps.Size(20, 30)),
-        전라: new kakao.maps.MarkerImage('img/m4.png', new kakao.maps.Size(20, 30)),
-        강원: new kakao.maps.MarkerImage('img/m5.png', new kakao.maps.Size(20, 30)), 
-        경상: new kakao.maps.MarkerImage('img/m6.png', new kakao.maps.Size(20, 30)),
+        경기: new kakao.maps.MarkerImage('img/m1.png', new kakao.maps.Size(18, 30)),
+        서울: new kakao.maps.MarkerImage('img/m5.png', new kakao.maps.Size(18, 30)),
+        충청: new kakao.maps.MarkerImage('img/m3.png', new kakao.maps.Size(18, 30)),
+        전라: new kakao.maps.MarkerImage('img/m4.png', new kakao.maps.Size(18, 30)),
+        강원: new kakao.maps.MarkerImage('img/m2.png', new kakao.maps.Size(18, 30)), 
+        경상: new kakao.maps.MarkerImage('img/m6.png', new kakao.maps.Size(18, 30)),
     };
 
     // 제로 웨이스트 샵 정보 반복해서 처리
