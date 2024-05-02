1ce0a8ab8d1a3c8cef283ac565df9afb0357de57
suyn04 <63181309+suyn04@users.noreply.github.com>
2024-05-02 15:49:33 +0900

    recycle

---
+++ public/recycle1.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const buttons = document.querySelectorAll(".recycleTab button");
+  const contents = document.querySelectorAll(".recycleContent div");
+
+  contents.forEach(content=>{
+    content.style.display = "none";
+  });
+
+  contents[0].style.display = "block";
+
+  buttons.forEach((button, index)=>{
+    button.addEventListener("click", function(){
+      const currentIndex = index;
+
+      buttons.forEach(btn=>{
+        btn.classList.remove("active");
+      });
+      button.classList.add("active");
+
+      contents.forEach(content=>{
+        content.style.display = "none";
+      });
+      contents[currentIndex].style.display = "block";
+    });
+  });
+});
