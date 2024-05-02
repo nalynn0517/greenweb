87808d1054a84a826a3014c41cac6ad371815177
suyn04 <63181309+suyn04@users.noreply.github.com>
2024-05-02 21:25:05 +0900

    recycle-btn(only)

--- public/recycle.js
+++ public/recycle.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
   const buttons = document.querySelectorAll(".recycleTab button");
   const contents = document.querySelectorAll(".recycleContent div");
+  const subBtns = document.querySelectorAll(".recycleSubTab li");
 
   contents.forEach(content=>{
     content.style.display = "none";
   });
-
+  subBtns.forEach(button=>{
+    button.style.display = "none";
+  });
+  
   contents[0].style.display = "block";
+  subBtns[0].style.display = "block";
 
   buttons.forEach((button, index)=>{
     button.addEventListener("click", function(){
@@ -21,6 +26,20 @@ document.addEventListener("DOMContentLoaded", function() {
         content.style.display = "none";
       });
       contents[currentIndex].style.display = "block";
+
+      subBtns.forEach(button=>{
+        button.style.display = "none";
+      });
+      subBtns[currentIndex].style.display = "block";
+
+      //subTab
+
+      //button click event listener
+
     });
   });
+// recycleTab
+
+
+  
 });
