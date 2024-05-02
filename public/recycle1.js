e10afaff1d4b5c9f34282a487b9471f7b21b93c3
suyn04 <63181309+suyn04@users.noreply.github.com>
2024-05-02 18:11:04 +0900

    recycle

--- public/recycle1.js
+++ public/recycle1.js
@@ -21,6 +21,37 @@ document.addEventListener("DOMContentLoaded", function() {
         content.style.display = "none";
       });
       contents[currentIndex].style.display = "block";
+
+      //subTab
+      const subTabCount = {
+        subTabBtn1 : 2,
+        subTabBtn2 : 3,
+        subTabBtn3 : 2,
+        subTabBtn4 : 8,
+        subTabBtn5 : 2,
+        subTabBtn6 : 2,
+        subTabBtn7 : 2,
+        subTabBtn8 : 2
+      };
+
+      const currentButtonId = button.getAttribute("id");
+      const subButtonCount = subTabCount[currentButtonId];
+
+      const recycleSubTab = document.querySelector("recycleSubTab");
+      recycleSubTab.innerHTML = "";
+
+      for(let i = 1; i <= subButtonCount; i++){
+        const subButton = document.createElement("button");
+        subButton.textContent = `Sub Button ${1}`;
+        recycleSubTab.appendChild(subButton);
+      }
+
+      //button click event listener
+
     });
   });
+// recycleTab
+
+
+  
 });
