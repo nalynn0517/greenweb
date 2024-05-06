a33f9da08938380833a075df9b2ac0490fb96365
suyn04 <63181309+suyn04@users.noreply.github.com>
2024-05-06 18:38:52 +0900

    recycle-subBtn setting(only)

--- public/recyclePage/recycle.js
+++ public/recyclePage/recycle.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   const buttons = document.querySelectorAll(".recycleTab button"); // 종이 등 버튼 불러오기
   const contents = document.querySelectorAll(".recycleContent>div"); // 내용부분 불러오기
-  const subBtns = document.querySelectorAll(".recycleSubTab li"); // 재활용가능,불가능 버튼 불러오기
+  const subBtns = document.querySelectorAll(".recycleSubTab li"); // 서브 버튼 불러오기
+  const subBtn = document.querySelectorAll(".recycleSubTab button"); // 서브버튼 각각 버튼 불러오기
+
 
   contents.forEach(content=>{
     content.style.display = "none";
@@ -30,16 +32,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
       subBtns.forEach(button=>{
         button.style.display = "none";
-        // button.classList.remove("active");
       });
       subBtns[currentIndex].style.display = "block";
 
-    
+      // 해당 메인 버튼의 첫 번째 서브버튼을 활성화
+      const firstSubBtn = subBtns[currentIndex].querySelector('button');
+      firstSubBtn.click();
 
     });
   });
 // recycleTab
+  subBtn.forEach((button, index) => {
+    button.addEventListener('click', function() {
+      const currentIndex = index;
 
+      subBtn.forEach(btn=>{
+          btn.classList.remove("active");
+      });
+      button.classList.add("active");
+    });
+  });
 
   
 });
