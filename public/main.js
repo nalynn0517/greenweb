71657a0275fd3874a193deb2956fcb01eed0f87a
suyn04 <63181309+suyn04@users.noreply.github.com>
2024-05-13 16:44:31 +0900

    update-master
    master에서 코드 업데이트

--- public/main.js
+++ public/main.js
@@ -1,10 +1,21 @@
+function setupLogoClick() {
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", function() {
+        window.location.href = "http://localhost:8080";
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+    // 각 페이지에서 로고 클릭 이벤트 설정
+    setupLogoClick();
+
     // 카카오맵 API 키
     const kakaoApiKey = '8948120168a32c7a96068b04adbb1558';
     
     // 카카오맵 API를 비동기로 로드
     loadKakaoMapAPI(kakaoApiKey);
 });
+
 
 // 카카오맵 API를 비동기로 로드하는 함수
 function loadKakaoMapAPI(apiKey) {
@@ -29,6 +40,15 @@ function initMap() {
     // 지도 생성
     const map = new kakao.maps.Map(container, options);
 
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+
+    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+    
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
     // 제로 웨이스트 샵 정보를 가져와서 표시
     showZeroWasteStores(map);
 }
@@ -40,12 +60,12 @@ function showZeroWasteStores(map) {
 
     // 제로 웨이스트 샵 마커 이미지
     const markerImages = {
-        경기: new kakao.maps.MarkerImage('img/m1.png', new kakao.maps.Size(20, 30)),
-        서울: new kakao.maps.MarkerImage('img/m2.png', new kakao.maps.Size(20, 30)),
-        충청: new kakao.maps.MarkerImage('img/m3.png', new kakao.maps.Size(20, 30)),
-        전라: new kakao.maps.MarkerImage('img/m4.png', new kakao.maps.Size(20, 30)),
-        강원: new kakao.maps.MarkerImage('img/m5.png', new kakao.maps.Size(20, 30)), 
-        경상: new kakao.maps.MarkerImage('img/m6.png', new kakao.maps.Size(20, 30)),
+        경기: new kakao.maps.MarkerImage('img/m1.png', new kakao.maps.Size(18, 30)),
+        서울: new kakao.maps.MarkerImage('img/m5.png', new kakao.maps.Size(18, 30)),
+        충청: new kakao.maps.MarkerImage('img/m3.png', new kakao.maps.Size(18, 30)),
+        전라: new kakao.maps.MarkerImage('img/m4.png', new kakao.maps.Size(18, 30)),
+        강원: new kakao.maps.MarkerImage('img/m2.png', new kakao.maps.Size(18, 30)), 
+        경상: new kakao.maps.MarkerImage('img/m6.png', new kakao.maps.Size(18, 30)),
     };
 
     // 제로 웨이스트 샵 정보 반복해서 처리
